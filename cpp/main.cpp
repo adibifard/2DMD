cee7429a6b25c38d19ced04c7c491bd6472b1ce9
adibifard <60307528+adibifard@users.noreply.github.com>
2022-04-18 19:55:25 -0500

    3rd

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,22 +1,43 @@
+#include <cmath>
+#include <vector>
+
 #include "MDfunctions.h"
 
+#define cutoff 1
 
 // This is the entering point of the program
+// Units: distance=nm, time=fs, 
 int main()
 {
 
-	int Na = 20; // the number of atoms
+	
+	
+	// Setup simulation time settings
+	double dt = 2; // in fs
+	double tf =20000 ;// in fs
+	int NTsteps = std::round(tf/dt);
+	
+	
 
-	int NTsteps = 100;
-	float dt = 1; // in fs
-
+	// Set box dimensions
+	double L = 5; 
+	int Na = 40; // the number of atoms
+	// Initialize atoms within the box
 	std::vector<atom> Atoms;
+
+	// Set binning properties
+	double alpha = 1;
+	double binSize = alpha * cutoff;
+	int Nb =std::ceil(L/binSize); // Number of bins in 1D
+	int NumBins=std::pow(Nb,2); //number of total bins
+	
+	std::vector<std::vector<int>> Bin; // initialize the Bin structure
 
 	// loop over the number of time steps
 	for (size_t i = 0; i <= NTsteps; i++)
 	{
 
-		// bin the particles (By Meisam)
+		// Divide particles into bins (By Meisam)
 
 
 		// Apply pair-wise forces (By Anishumitra)
@@ -24,7 +45,7 @@ int main()
 		{
 			for (size_t j = 0; j < Na; j++) // this is an O(n^2) implementation that we will need to change it after implementing the Binning function into the code
 			{
-				ApplyForce(Atoms[i], Atoms[j]); // Please complete the vel-verlet function
+				ApplyForce(Atoms[i], Atoms[j]); // Please complete the ApplyForce function
 			}
 		}
 
