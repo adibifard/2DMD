36f0b2a5b9fe484703d6023f9e660ef65882c073
adibifard <60307528+adibifard@users.noreply.github.com>
2022-04-30 02:01:48 -0500

    update (PBCimageBox,binning,neighboring)

--- cpp/MDfunctions.cpp
+++ cpp/MDfunctions.cpp
@@ -1,9 +1,11 @@
 #include "MDfunctions.h"
 #include <random>
-#include <math.h>
-#include <stdlib.h>
+#include <span>
+#include <fstream>
+
 
 using std::pow;
+double LBox;
 // This is the cpp file used to code the essential functions needed for our 2DMD simulator
 
 
@@ -11,35 +13,37 @@ using std::pow;
 // A function to initialize the atoms' positions
 void InitAtomsPos(std::vector<atom>& Atoms, double LBox, int Na)
 {
-// Uniform distribution of the atoms
+	// Uniform distribution of the atoms
 //	for (size_t i=0;i<Atoms.size();i++)
 //	{
-//
-
-	int sx = (int)ceil(sqrt((double)Na));
-	int sy = (Na + sx - 1) / sx;
-
-	int* shuffle = (int*)malloc(Na * sizeof(int));
-	for (int i = 0; i < Na; i++)
-		shuffle[i] = i;
-
-	for (int i = 0; i < Na; i++)
-	{
-		//
-		//  particles are not spatially sorted
-		//
-		long int lrand48(void);
-		int j = lrand48()%(Na - i);
-		int k = shuffle[j];
-		shuffle[j] = shuffle[Na - i - 1];
-
-		//
-		//  particles distributed uniformly
-		//
-		Atoms[i].pos = {LBox * (1. + (k % sx)) / (1 + sx), LBox * (1. + (k / sx)) / (1 + sy)};
-
+//		
+//	}
+	int n, p, i, j;
+	double a;
+	//
+	//   // Number of atoms in each direction
+	n = int(ceil(pow(Atoms.size(), 1.0 / 3)));
+	//
+	//   //  spacing between atoms along a given direction
+	a = LBox / n;
+	//   
+	//   //  index for number of particles assigned positions
+	p = 0;
+	//   //  initialize positions
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			if (p < Na) {
+				Atoms[p].pos = { (i)*a,(j)*a }; //atom at the origin
+					p = p + 1;
+				Atoms[p].pos = { (i)*a,(j + 0.5) * a }; //atom at one edge
+					p = p + 1;
+				Atoms[p].pos = { (i + 0.5) * a,(j)*a }; //atom at the other edge
+					p = p + 1;
+				Atoms[p].pos = { (i + 0.5) * a,(j + 0.5) * a }; //atom diagonal from the origin
+					p = p + 1;
+			}
+		}
 	}
-	free(shuffle);
 }
 
 // A function to initialize the atoms' velocities using Maxwell-Boltzmann distribution at the set-point temperature
@@ -48,9 +52,10 @@ void InitAtomsVel(std::vector<atom> &Atoms, double T, int Na)
 	std::random_device myseed;
 	std::mt19937 Myengine(myseed());
 	std::vector<double> velMag;
+	double kBmod = (kB * kg2gr * pow(m2A, 2)) / pow(s2fs, 2);
 	for (size_t i = 0; i < Na; i++)
 	{
-		double SIG = std::sqrt(kB * T / Atoms[i].m);
+		double SIG = std::sqrt(kB_ru * T / (Atoms[i].m/ Navg));
 		double Mean = 0;
 		std::normal_distribution<double> NormalDist(Mean, SIG);
 
@@ -64,63 +69,170 @@ void InitAtomsVel(std::vector<atom> &Atoms, double T, int Na)
 // This function determines the size of the box for n number of atoms
 double setBoxSize(const double density, const int N)
 {
-	double L = std::sqrt(density * N);
-	return L;
+	LBox = std::sqrt( N/ density);
+	return LBox;
 }
 
 // This function determines the bining index of each particle 
-void BinParticles(atom &particle,const double BinSize)
+void BinParticles(std::vector<atom>& Atoms, const double BinSize, std::vector<std::vector<std::vector<int>>>& Bin)
 {
-	TwoDvec<int> tmpbinIJ = particle.pos.ceil(BinSize);
-	particle.binIJ = tmpbinIJ;
+	for (int i=0;i<Atoms.size(); i++)
+	{
+		int I = std::floor(Atoms[i].pos[0] / BinSize);
+		int J = std::floor(Atoms[i].pos[1] / BinSize);
+		Atoms[i].binIJ = { I,J };
+		Bin[I][J].push_back(i);
+	}	
 	
 }
 
-
-// This function calculates the pair-wise force between the i-th and the j-th particle
-void ApplyForce(atom& Atoms_i, atom Atoms_j, double sig, double eps)
+void PBC_images::ReturnImageBoxes (std::vector<atom> Atoms)
 {
-	// Lennard-Jone Potential
+	int Natoms = Atoms.size();
+	XplusImage.resize(Natoms); XminusImage.resize(Natoms); YplusImage.resize(Natoms); YminusImage.resize(Natoms);
+	XYplusImage.resize(Natoms); XYminusImage.resize(Natoms); XplusYminusImage.resize(Natoms); XminusYplusImage.resize(Natoms);
 
-	// Magnitude of the force between particles i and j 
-	TwoDvec<double> rij = Atoms_i.pos - Atoms_j.pos;
-	double r = rij.norm();
-	// The direction of the force between particles i and j 
-	TwoDvec<double> nij = rij / r;
-	
-	double Fij = 4 * eps * (12 * pow(sig, 12) / pow(r, 13) - 6 * pow(sig, 6) / pow(r, 7));
+	for (size_t i = 0; i < Natoms; i++)
+	{
+		XplusImage[i].pos[0] = Atoms[i].pos[0] + LBox; XplusImage[i].pos[1] = Atoms[i].pos[1];
+		XminusImage[i].pos[0] = Atoms[i].pos[0] - LBox; XminusImage[i].pos[1] = Atoms[i].pos[1];
+		YplusImage[i].pos[0] = Atoms[i].pos[0]; YplusImage[i].pos[1] = Atoms[i].pos[1] + LBox;
+		YminusImage[i].pos[0] = Atoms[i].pos[0]; YminusImage[i].pos[1] = Atoms[i].pos[1] - LBox;
+		XYplusImage[i].pos[0] = Atoms[i].pos[0] + LBox; XYplusImage[i].pos[1] = Atoms[i].pos[1] + LBox;
+		XYminusImage[i].pos[0] = Atoms[i].pos[0] - LBox; XYminusImage[i].pos[1] = Atoms[i].pos[1] - LBox;
+		XplusYminusImage[i].pos[0] = Atoms[i].pos[0] + LBox; XplusYminusImage[i].pos[1] = Atoms[i].pos[1] - LBox;
+		XminusYplusImage[i].pos[0] = Atoms[i].pos[0] - LBox; XminusYplusImage[i].pos[1] = Atoms[i].pos[1] + LBox;
+	}
+}
 
-	// Update the force components for the i-th particle in 2D
-	Atoms_i.f = Atoms_i.f - nij * Fij;
+// This function determines the neighboring list per atom
+void Neighboring(std::vector<atom>& Atoms, std::vector<std::vector<std::vector<int>>> Bin)
+{
+	// iterate over the bins
+	for (int i=0;i<Bin.size();i++)
+	{
+		for (int j=0;j<Bin[0].size();j++)
+		{
+			for (int k = 0; k < Bin[i][j].size(); k++) 
+			{
+				int atomID = Bin[i][j][k];
+				Atoms[atomID].NeighbIndex.insert(Atoms[atomID].NeighbIndex.end(), Bin[i][j].begin(), Bin[i][j].end()); // (i,j)
+
+				if (i > 0) Atoms[atomID].NeighbIndex.insert(Atoms[atomID].NeighbIndex.end(), Bin[i-1][j].begin(), Bin[i-1][j].end()); // (i-1, j)
+				if (i< Bin.size()-1) Atoms[atomID].NeighbIndex.insert(Atoms[atomID].NeighbIndex.end(), Bin[i+1][j].begin(), Bin[i+1][j].end()); // (i+1, j)
+
+				if (j>0) Atoms[atomID].NeighbIndex.insert(Atoms[atomID].NeighbIndex.end(), Bin[i][j-1].begin(), Bin[i ][j-1].end()); // (i, j-1)
+				if (j< Bin.size() - 1) Atoms[atomID].NeighbIndex.insert(Atoms[atomID].NeighbIndex.end(), Bin[i ][j+1].begin(), Bin[i ][j+1].end()); // (i, j+1)
+
+				if (i < Bin.size() - 1 && j < Bin.size() - 1 ) Atoms[atomID].NeighbIndex.insert(Atoms[atomID].NeighbIndex.end(), Bin[i+1][j + 1].begin(), Bin[i+1][j + 1].end());// (i+1, j+1)
+				if (i > 0 && j>0) Atoms[atomID].NeighbIndex.insert(Atoms[atomID].NeighbIndex.end(), Bin[i - 1][j-1].begin(), Bin[i - 1][j-1].end());// (i-1, j-1)
+
+				if (i < Bin.size() - 1 && j >0) Atoms[atomID].NeighbIndex.insert(Atoms[atomID].NeighbIndex.end(), Bin[i + 1][j - 1].begin(), Bin[i + 1][j - 1].end());// (i+1, j-1)
+				if (i>0 && j < Bin.size() - 1) Atoms[atomID].NeighbIndex.insert(Atoms[atomID].NeighbIndex.end(), Bin[i - 1][j + 1].begin(), Bin[i - 1][j + 1].end());// (i-1, j+1)
+			}
+		}
+	}
+}
+
+// A function to determine the fi-- forces acting over the i-th atom
+void ApplyForce(std::vector<atom>& Atoms,double eps, double sig)
+{
+	for (int i=0;i<Atoms.size();i++)
+	{
+		Atoms[i].f = { 0,0 };
+		for (int j=0;j<Atoms[i].NeighbIndex.size();j++)\
+		{
+			int Indj = Atoms[i].NeighbIndex[j];
+			// Magnitude of the force between particles i and j 
+			TwoDvec<double> rij = Atoms[i].pos - Atoms[Indj].pos;
+
+			double r = rij.norm();
+			if (r != 0 && r <= rcut_off)
+			{
+				// The direction of the force between the particles i and j 
+				TwoDvec<double> nij = rij / r;
+
+				double Fij = 4 * eps * (12 * pow(sig, 12) / pow(r, 13) - 6 * pow(sig, 6) / pow(r, 7));
+
+				// Update the force components for the i-th particle in 2D
+				Atoms[i].f = Atoms[i].f - nij * Fij;
+			}
+		}
+	}
+}
+
+// A function to impose the PBC
+void PBC(double &x)
+{
+
+	if (x < 0)
+	{
+		x+= LBox;
+	}
+	else if (x > LBox)
+	{
+		x-= LBox;
+	}
 
 }
+
 
 
 // This function is applied over each particle separately, after force calculations.
 // Basically, this function updates the current position and velocity of the particle using the calculated force per particle. 
-void VelVerlt(atom &Atom, double dt) 
+void Euler(atom &Atom, double dt)
 {
 	//  Compute accelerations from forces at current position
+	double KcaltoJoule = 4186.8;
+	double Jtorealunits = 1e-7;
+	double unitConv = KcaltoJoule * Jtorealunits;
+	// Semi-implicit Euler integration
+	Atom.v = Atom.v + (Atom.f / Atom.m) * dt* unitConv;
+	Atom.pos = Atom.pos + Atom.v * dt;
 
-	// Euler's algorithm
-	Atom.pos = Atom.pos + Atom.v * dt + Atom.f * (pow(dt, 2) / (2 * Atom.m));
-
-	Atom.v = Atom.v + Atom.f * (dt / Atom.m);
-	//periodic boundary condition
-	Atom.pos += Atom.v*dt +Atom.f*0.5 * dt * dt / Atom.m;
-	//if (Atom.pos < 0) {
-	//		Atom.pos = Atom.pos+LBox;
-         //}
-	//if (Atom.pos > LBox) {
-	//	Atom.pos = Atom.pos-LBox;
-	//	}
+	// Apply Periodic Boundary Condition over the position of the particles leaving the box boundaries
+	PBC(Atom.pos[0]);
+	PBC(Atom.pos[1]);
   
+}
+
+// Velocity Verlet
+void VelVerlet(std::vector<atom>& Atoms, double dt, double eps, double sig)
+{
+	//  Compute accelerations from forces at current position
+	double KcaltoJoule = 4186.8;
+	double Jtorealunits = 1e-7;
+	double unitConv = KcaltoJoule * Jtorealunits;
+
+	for (atom &atom1 : Atoms) 
+	{
+		// compute a(t)
+		atom1.a0 = (atom1.f / atom1.m) * unitConv;
+		// compute x(t+dt)
+		atom1.pos = atom1.pos + atom1.v * dt + atom1.a0 * pow(dt, 2)*0.5;
+	}
+
+	// Update force at t+dt
+	ApplyForce(Atoms, eps, sig);
+
+	for (atom &atom1 : Atoms)
+	{
+		// compute a(t+dt)
+		atom1.a1 = (atom1.f / atom1.m) * unitConv;
+		// compute v(t+dt)
+		atom1.v = atom1.v + (atom1.a0 + atom1.a1) * dt * 0.5;
+		// Apply Periodic Boundary Condition over the position of the particles leaving the box boundaries
+		PBC(atom1.pos[0]);
+		PBC(atom1.pos[1]);
+	}
+
+	
+
 }
 
 
 
 // The thermostat function
-
 
 
 // Calculate KE
@@ -149,4 +261,38 @@ double CalcInstanPE(std::vector<atom> Atoms)
 }
 
 
+// Write data into an excel file
+void WriteToExcel(std::string filename, std::vector<atom>  data, std::string colName,int Dtype)
+{
+	std::fstream fout;
 
+
+	fout.open(filename, std::ios::out | std::ios::app);
+
+	fout << colName << "\n";
+
+	if (Dtype==0) // Dump vx
+	{
+		for (auto atom : data)
+		{
+			fout << atom.v[0] << "\n";
+		}
+	}
+	
+	if (Dtype == 1) // Dump x
+	{
+		for (auto atom : data)
+		{
+			fout << atom.pos[0] << "\n";
+		}
+	}
+
+	if (Dtype == 2) // Dump y
+	{
+		for (auto atom : data)
+		{
+			fout << atom.pos[1] << "\n";
+		}
+	}
+
+}
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,10 +1,10 @@
 #include <cmath>
 #include <vector>
-
+#include <string>
 #include "MDfunctions.h"
 
-#define cutoff 1
-
+#define cutoff 10
+#define ns2fs 1e+6
 // This is the entering point of the program
 // Units: distance=nm, time=fs, Energy=Kj, 
 // Simulation of an NVT ensemble with Periodic Boundary Conditions
@@ -12,20 +12,21 @@ int main()
 {
 	// Setup simulation time settings
 	double dt = 2; // in fs
-	double tf =20000 ;// in fs
-	size_t NTsteps = std::round(tf/dt);
+	double tf =50 ;// in ns
+	size_t NTsteps = std::round(tf* ns2fs /dt);
 	
-	// Set forcefield (LJ) parameters
-	double sigma =1 ; // in nm
-	double eps =1 ;   // 
-	double mass = 1;
+	// Set forcefield (LJ) parameters (Argon-> from ATB molecular structures)
+	double sigma = 3.409840044152281813;	 // in A
+	double eps = 2.381475572067369983E-01;   // Kcal/mole
+	double mass = 39.948000000;				// gr/mole
 
 	// Set the number of atoms in the box
 	size_t Na = 100; // the number of atoms
-	double density = 1; //  #atoms/area
+	double density = 0.026; //  #atoms/area ro(Ar)=1.784 gr/cm3 -> 1.784 gr/cm2
 	// Allocate memory for atoms
 	std::vector<atom> Atoms(Na);
-
+	
+	
 	for (size_t i = 0; i < Atoms.size(); i++)
 		Atoms[i].m = mass;
 
@@ -37,44 +38,78 @@ int main()
 	double Nb = std::ceil(LBox / binSize); // Number of bins in 1D
 	int NumBins=std::pow(Nb,2); //number of total bins
 	
-	std::vector<std::vector<int>> Bin; // initialize the Bin structure
-
+	std::vector<std::vector<std::vector<int>>> Bin(Nb); // initialize the Bin structure
+	for (int i = 0; i < Nb; i++) {
+		Bin[i].resize(Nb);
+	}
+	// Declare a class of images
+	PBC_images IMGBox;
 	// Initialize Atoms' velocities
-	double T = 300; // Kelvin
+	double T = 350; // Kelvin
 	InitAtomsPos(Atoms, LBox, Na);
 	InitAtomsVel(Atoms,T,Na);
-
+	WriteToExcel("Vx_t0.csv", Atoms, "Vx(A/fs)",0);
+	//WriteToExcel("x0.csv", Atoms, "x(A)",1);
+	//WriteToExcel("y0.csv", Atoms, "y(A)", 2);
 	// loop over the number of time steps
 	for (size_t i = 0; i <= NTsteps; i++)
 	{
 
 		// Divide particles into bins (By Meisam)
-		for (size_t i = 0; i < Na; i++) 
-		{
-			BinParticles(Atoms[i], binSize);
-		}
+		BinParticles(Atoms, binSize, Bin);
 
+		// Populate the image boxes
+		IMGBox.ReturnImageBoxes(Atoms);
+		
 
 		// Apply pair-wise forces (By Meisam)
-		for (size_t i = 0; i < Na; i++)
+
+		Neighboring(Atoms, Bin);
+
+		ApplyForce(Atoms, eps, sigma);
+
+		VelVerlet(Atoms, dt, eps, sigma);
+
+		// Move the particles using velocity-Verlet algorithm (By Anishumitra)
+		//for (size_t i = 0; i < Na; i++)
+		//{
+		//	Euler(Atoms[i],dt); // Please complete the vel-verlet function
+		//}
+
+
+		// Remove current binning
+		for (int i = 0; i < Bin.size(); i++)
 		{
-			Atoms[i].f = { 0,0 };
-			for (size_t j = 0; j < Na; j++) // this is an O(n^2) implementation that we will need to change it after implementing the Binning function into the code
+			for (int j=0;j<Bin[0].size();j++)
 			{
-				ApplyForce(Atoms[i], Atoms[j],eps,sigma); //  completed
+				Bin[i][j].clear();
 			}
 		}
 
+		/*std::string fileName="Vx_t" + std::to_string(i+1) +".csv";
+		WriteToExcel(fileName, Atoms, "Vx(A/fs)",0);*/
 
-		// Move the particles using velocity-Verlet algorithm (By Anishumitra)
-		for (size_t i = 0; i < Na; i++)
-		{
-			VelVerlt(Atoms[i],dt); // Please complete the vel-verlet function
-		}
-
+		/*std::string XfileName = "x_t" + std::to_string(i + 1) + ".csv";
+		std::string YfileName = "y_t" + std::to_string(i + 1) + ".csv";
+		WriteToExcel(XfileName, Atoms, "x(A)", 1);
+		WriteToExcel(YfileName, Atoms, "y(A)", 2);*/
 		// Determine the instantaneous pressure and temperature of the system (By Meisam and Anishumitra)
-		double KineticEnergy = CalcInstanKE(Atoms);
-		double Tinst = KineticEnergy / (Na * kB);
+		double KineticEnergy = CalcInstanKE(Atoms); 
+		double KcaltoJoule = 4186.8;
+		double KE_KcalPerMole = KineticEnergy * s2fs / (kg2gr * m2A* KcaltoJoule); // Kcal/mole
+		double PEnergy = CalcInstanPE(Atoms);
+		double TotalEn = KineticEnergy + PEnergy;
+		std::cout <<i << "KE: " << KE_KcalPerMole << "\n";
+		if (i == 199) {
+			int p = 1;
+		}
+		//std::cout << "Total Energy (Kcal/mole): " << TotalEn << "\n";
+		//std::cout << "Time (fs): "<<(i+1)*dt<<"-PE (Kcal/mole): " << PEnergy <<"-KE (Kcal/mole): "<< KE_KcalPerMole<< "\n";
+		double kBmod = (kB * kg2gr * pow(m2A, 2)) / pow(s2fs, 2);
+
+		double Tinst = KineticEnergy  / (Navg* kB_ru);
+
+		std::cout << "Tinst: " << Tinst << "\n";
 	}
 
 	return 0;
